73db0e042a619c45c6e934b3d559b86d4d016ebb
Your Name <you@example.com>
2017-09-25 16:02:23 +0300

    4

--- templates/index.php
+++ templates/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
     <meta charset="utf-8">
-    <title> Task4 </title>
+    <title> Task11 </title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-theme.min.css" rel="stylesheet">
@@ -10,35 +10,41 @@
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->    
-    </head>
-    <body>
-        <form action="index.php" method="POST">
-            <div class="panel panel-default">
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->    
+</head>
+<body>
+<form action="index.php" method="POST">
+<div class="panel panel-default">
 
-                <!-- Default panel contents -->
-                <div class="panel-heading">
-                    MySQL
-                    <p><?php echo $msgMy ?></p>
-                </div>
+<!-- Default panel contents -->
+<div class="panel-heading">
+MySQL
+<p><?php echo $msgMy ?></p>
+</div>
 
-                <!-- Table -->
-                <table class="table">
-                        <tr>
-                          <td style="width:20%">User01</td>
-                          <td><?php echo $result ?></td>
-                        </tr>
-                 </table>
-            </div>
-    
-            <div class="btn-group" role="group" aria-label="..." style="margin-bottom: 5%">
-                <button type="submit" name="insert" value="insert" class="btn btn-default">Insert</button>
-                <button type="submit" name="update" value="update" class="btn btn-default">Update</button>
-                <button type="submit" name="delete" value="delete" class="btn btn-default">Delete</button>
-            </div> 
-                                                                         
-        </form>
-    </body>
+<!-- Table -->
+<table class="table">
+<tr>
+<td style="width:20%">User01</td>
+<td><?php echo $result ?></td>
+</tr>
+</table>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-default">Insert</button>
+  <button type="button" class="btn btn-default">Update</button>
+  <button type="button" class="btn btn-default">Delete</button>
+</div>
+
+<!--
+<div class="btn-group" role="group" aria-label="..." style="margin-bottom: 5%">
+<button type="submit" name="insert" value="insert" class="btn btn-default">Insert</button>
+<button type="submit" name="update" value="update" class="btn btn-default">Update</button>
+<button type="submit" name="delete" value="delete" class="btn btn-default">Delete</button>
+</div> 
+-->
+</form>
+</body>
 </html>
